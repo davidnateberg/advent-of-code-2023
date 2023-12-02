891a725362ce30363ca804614f88bb8088c51f37
David Berg <davidnateberg@gmail.com>
2023-12-02 16:20:03 -0600

    day1 code

--- day1/calculate2.py
+++ day1/calculate2.py
@@ -1,41 +1,49 @@
-import re
-
 with open('calibrations.txt') as calibrations:
     data = calibrations.read().splitlines()
     total = 0
-    num = 1
+    data_cleaned = []
+
     nums_to_replace = {
-        'one': '1', 
-        'two': '2', 
+        'one': 'o1e', 
+        'two': 't2o', 
         'three': '3', 
         'four': '4', 
         'five': '5',
         'six': '6',
         'seven': '7',
         'eight': '8',
-        'nine': '9'}
+        'nine': '9'
+    }
+    line_number = 1
     for line in data:
-
-        print(f'Old line: {line}')
         
         for key, value in nums_to_replace.items():
             line = line.replace(key, value)
-        
-        print(f'Converted: {line}')
-        
-        nums_in_line = list(filter(lambda x: x.isdigit(), line))
-        first_num = nums_in_line[0]
-        last_num = nums_in_line[-1]
+    
+        line_number = line_number + 1
+        print(f'line number {line_number}')    
+        only_numbers = list(filter(lambda x: x.isdigit(), line))
+        data_cleaned.append(only_numbers)
 
-        print(f'numbers in line {nums_in_line}')
+    print(data_cleaned)
+
+    line_number = 1
+    for line in data_cleaned:
+        print(f'--line number {line_number}--')
+        
+        first_num = line[0]
+        last_num = line[-1]
                 
-        if len(nums_in_line) >= 2:
+        if len(line) >= 2:
             sum = first_num + last_num
             total = int(sum) + total
+            print(line)
             print(f'calibration: {sum}')
+            line_number = line_number + 1
         else:
             sum = first_num + first_num
             total = int(sum) + total
             print(f'calibration: {sum}')
+            line_number = line_number + 1
 
     print(f'The total is {total}')
